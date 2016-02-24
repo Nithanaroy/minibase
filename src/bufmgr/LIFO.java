c4a73a7f81fea6b49f6e7d6475b99f604ef8136a
Nitin Pasumarthy <nithanaroy@gmail.com>
2016-02-23 21:55:22 -0700

    Implemented LIFO Buffer Replacement Policy
    Buffer Manager class doesnt have an entry point yet to LIFO. This is added in the next commit along with LRUK

---
+++ src/bufmgr/LIFO.java
@@ -0,0 +1,56 @@
+package bufmgr;
+
+import java.util.ArrayList;
+
+public class LIFO extends Replacer {
+
+	private ArrayList<Integer> frames;
+	private int nframes;
+
+	protected LIFO(BufMgr mgr) {
+		super(mgr);
+		frames = null;
+	}
+
+	@Override
+	protected void setBufferManager(BufMgr mgr) {
+		super.setBufferManager(mgr);
+		frames = new ArrayList<Integer>(mgr.getNumBuffers());
+		nframes = 0;
+	};
+
+	@Override
+	public int pick_victim() throws BufferPoolExceededException, PagePinnedException {
+		int numBuffers = mgr.getNumBuffers();
+		int frame;
+
+		if (nframes < numBuffers) {
+			frame = nframes++;
+			frames.add(frame);
+			state_bit[frame].state = Pinned;
+			(mgr.frameTable())[frame].pin();
+			return frame;
+		}
+
+		for (int i = numBuffers - 1; i >= 0; ++i) {
+			frame = frames.get(i);
+			if (state_bit[frame].state != Pinned) {
+				state_bit[frame].state = Pinned;
+				(mgr.frameTable())[frame].pin();
+				frames.add(frames.remove(i)); // Move this frame to the end
+				return frame;
+			}
+		}
+
+		return -1;
+	}
+
+	/**
+	 * Page replacement policy name
+	 */
+	@Override
+	public String name() {
+		return "LIFO";
+	}
+
+}
