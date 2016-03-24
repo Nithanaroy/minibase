d02f1a67e572244dda0596b6ef72199abb8499ae
Nitin Pasumarthy <nithanaroy@gmail.com>
2016-03-23 23:07:07 -0700

    Added package info for readInput.java and capatalized filename

--- src/tests/readInput.java
+++ src/tests/readInput.java
@@ -1,70 +1,63 @@
+package tests;
+
 import java.io.*;
-import java.util.List;    
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+public class ReadInput {
 
-public class readInput {
+	public enum Predicate {
+		singlePredicate, doublePredicate, unknown;
+	}
 
-   public enum Predicate {
-      singlePredicate, doublePredicate,unknown;
-   }
+	public static void main(String args[]) throws IOException {
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		String line = null;
+		String fileName = "query_1a.txt";
+		List<String> queryList = new ArrayList<String>();
+		try {
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			while ((line = bufferedReader.readLine()) != null) {
+				queryList.add(line);
+			}
+			// Always close files.
+			bufferedReader.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + fileName + "'");
+		} catch (IOException ex) {
+			System.out.println("Error reading file '" + fileName + "'");
+			// Or we could just do this:
+			// ex.printStackTrace();
+		}
 
+		int listSize = queryList.size();
+		for (int i = 0; i < listSize; ++i)
+			System.out.println(queryList.get(i));
 
-   public static void main(String args[]) throws IOException
-   {
-      FileInputStream in = null;
-      FileOutputStream out = null;
-      String line = null;
-      String fileName = "query_1a.txt";
-      List<String> queryList = new ArrayList<String>();
-      try {
-         FileReader fileReader =  new FileReader(fileName);
-         BufferedReader bufferedReader =  new BufferedReader(fileReader);
-         while((line = bufferedReader.readLine()) != null) {
-                queryList.add(line);
-            }   
-            // Always close files.
-            bufferedReader.close();        
-      }
-      catch(FileNotFoundException ex) {
-            System.out.println("Unable to open file '" + fileName + "'");
-      }
-      catch(IOException ex) {
-            System.out.println("Error reading file '" + fileName + "'");
-            // Or we could just do this: 
-            // ex.printStackTrace();
-      }
+		// output fields + offsets
+		String output = queryList.get(0);
+		String[] data = output.split(" ");
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(data[i]);
+			String[] results = data[i].split("_");
+			System.out.println(Arrays.toString(results));
+		}
 
-      int listSize = queryList.size();
-      for(int i = 0; i < listSize; ++i)
-         System.out.println(queryList.get(i));
+		String[] filesToRead = queryList.get(1).split(" ");
 
-      // output fields + offsets
-      String output = queryList.get(0);
-      String[] data = output.split(" ");
-      for (int i=0;i<data.length;i++)
-      {
-         System.out.println(data[i]);
-         String[] results = data[i].split("_");
-         System.out.println(Arrays.toString(results));
-      }
-
-      String[] filesToRead = queryList.get(1).split(" ");
-
-      Predicate predicateType = Predicate.unknown;
-      if(queryList.size() == 3 ){
-         // Single predicate query
-         predicateType = Predicate.singlePredicate;
-      }
-      else if (queryList.size() == 5)
-      {
-         // Double predicate query  
-         predicateType = Predicate.doublePredicate;
-      }
-      else{
-         //unknown predicate query
-         predicateType = Predicate.unknown;
-      }
-   }
+		Predicate predicateType = Predicate.unknown;
+		if (queryList.size() == 3) {
+			// Single predicate query
+			predicateType = Predicate.singlePredicate;
+		} else if (queryList.size() == 5) {
+			// Double predicate query
+			predicateType = Predicate.doublePredicate;
+		} else {
+			// unknown predicate query
+			predicateType = Predicate.unknown;
+		}
+	}
 }
